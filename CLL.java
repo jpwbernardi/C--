a4bb5a87f7c5f818796aee6f8eff6f078a1c1366
Aluno (local) <s>
2015-04-01 18:07:14 -0300

    Codigo do exemplo fornecido pelo professor

--- CLL.java
+++ CLL.java
@@ -3,8 +3,19 @@ import java.io.*;
 
 class CLL{
 	public static void main(String args[]) throws Exception {
-        File f;
+        Scanner s;
+        Interpretador b;
+        String linhas[] = new String[2000];
         f = new File(args[0]);
-        
+        s = new Scanner(f);
+        b = new Interpretador();
+
+        int i = 0;
+        while(s.hasNext()) {
+            linhas[i] = s.nextLine();
+            i++;
+        }
+
+        b.interpreta(linhas);
     }
 }
