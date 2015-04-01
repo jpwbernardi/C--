b3a6c12a07bfbcac74b5155601d484b93bfd48c1
Aluno (local) <s>
2015-04-01 18:04:34 -0300

    Classe Interpretador adicionada

---
+++ Interpretador.java
@@ -0,0 +1,14 @@
+class Interpretador{
+	private String linhas[];
+
+    public void interpreta(String l[]) {
+        this.linhas = l;
+
+        for(int i = 0; i < this.linhas.length; i++) {
+            if(this.linhas[i] != null) {
+                // TODO: interpretar a linha
+                System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
+            }
+        }
+    }
+}
