65488081faea296852afa787920003865681d2c0
Aluno (local) <s>
2015-04-01 18:01:44 -0300

    Classe CLL criada

---
+++ CLL.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.io.*;
+
+class CLL{
+	public static void main(String args[]) throws Exception {
+        File f;
+        f = new File(args[0]);
+        
+    }
+}
