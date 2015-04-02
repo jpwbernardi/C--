8f9582cec3c7331e71c15068f542c6797a4a95a1
Aluno (local) <s>
2015-04-02 14:13:40 -0300

    Função divide adicionado no Interpretador.
    Novas classes:
    	Atribuiçao
    	Simbolos

--- Interpretador.java
+++ Interpretador.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Interpretador{
 	private String linhas[];
-	HashMap<String, Variavel> vars;
+	public static HashMap<String, Variavel> vars;
 
 	public Interpretador(){
 		vars = new HashMap<String, Variavel>();
@@ -15,14 +15,42 @@ class Interpretador{
 	public Variavel getVar(String n) {
 		return this.vars.get(n);
 	}
+	
+	private String[] divide(String n){
+		ArrayList<String> sequencia = new ArrayList<String>();
+		
+		String[] quebrado = n.split("");		//Quebra string n e guarda em quebrado
+		String aux = null;
+		
+		int i;
+		
+		for(i = 0; i < quebrado.length; i++){
+			if(aux == null) aux = new String("");
+			if(quebrado[i].equals("\n")){
+				sequencia.add(aux);
+				aux = null;
+			}else if(Simbolos.pertence(quebrado[i]) > 0){
+				sequencia.add(aux);
+				sequencia.add(quebrado[i]);	
+				aux = null;
+			}else if(quebrado[i].equals(" ") == false){
+				aux += quebrado[i];
+			}
+		}
+		String[] t = new String[sequencia.size()];
+		sequencia.toArray(t);
+		return t;
+	}
 
     public void interpreta(String l[]) {
         this.linhas = l;
 
         for(int i = 0; i < this.linhas.length; i++) {
             if(this.linhas[i] != null) {
-                // TODO: interpretar a linha
-                System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
+                String[] teste = this.divide(linhas[i]);
+                
+                for(String x: teste)
+					System.out.println(x);
             }
         }
     }
